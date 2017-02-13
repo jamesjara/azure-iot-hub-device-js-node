8dba37852d7dabbd9ea125281040355f7cc8873c
jamesjara <jamesjara@gmail.com>
2017-02-13 01:54:08 +0000

    SAS token generator polyfull but not working as usually MS stuff

--- CreateDeviceIdentity.js
+++ CreateDeviceIdentity.js
@@ -1,12 +1,19 @@
 'use strict';
 
 var iothub = require('azure-iothub');
+var crypto = require('crypto');
 
-var connectionString = 'HostName=test-jamesjara.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=oVoqeN/GGE9qmdPCYBMQhLM82EkM+U7bCICSBq4UB7E=';
-var registry = iothub.Registry.fromConnectionString(connectionString);
+var connectionString = 'HostName=thermo.azure-devices.net;SharedAccessKeyName=registryReadWrite;SharedAccessKey=uEzyH+a+dzEvldwsOW/iS1UXqk2e7BgT+Et2wXcMh2U=';
+
+var endpoint ="thermo.azure-devices.net"
+var policyName = 'registryReadWrite';
+var policyKey = 'uEzyH+a+dzEvldwsOW/iS1UXqk2e7BgT+Et2wXcMh2U=';
+var token = generateSasToken(endpoint, policyKey, policyName, 60);
+
+var registry = iothub.Registry.fromConnectionString(connectionString+token);
 
 var device = new iothub.Device(null);
-device.deviceId = 'myFirstNodeDevice';
+device.deviceId = 'aja';
 registry.create(device, function(err, deviceInfo, res) {
   if (err) {
     registry.get(device.deviceId, printDeviceInfo);
@@ -17,8 +24,28 @@ registry.create(device, function(err, deviceInfo, res) {
 });
 
 function printDeviceInfo(err, deviceInfo, res) {
+  console.log(err, deviceInfo, res);
   if (deviceInfo) {
     console.log('Device ID: ' + deviceInfo.deviceId);
     console.log('Device key: ' + deviceInfo.authentication.symmetricKey.primaryKey);
   }
 }
+
+
+function generateSasToken(resourceUri, signingKey, policyName, expiresInMins) {
+  resourceUri = encodeURIComponent(resourceUri);
+
+  // Set expiration in seconds
+  var expires = (Date.now('2018.08.10') / 1000) + expiresInMins * 60;
+  expires = Math.ceil(expires);
+  var toSign = resourceUri + '\n' + expires;
+  // Use crypto
+  var hmac = crypto.createHmac('sha256', new Buffer(signingKey, 'base64'));
+  hmac.update(toSign);
+  var base64UriEncoded = encodeURIComponent(hmac.digest('base64'));
+  // Construct autorization string
+  var token = "SharedAccessSignature sr=" + resourceUri + "&sig="
+  + base64UriEncoded + "&se=" + expires;
+  if (policyName) token += "&skn="+policyName;
+  return token;
+};
---
+++ simulateThermo/SimulatedThermoDevice.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString;
+var Message = require('azure-iot-device').Message;
+
+var connectionString = 'HostName=test-jamesjara.azure-devices.net;DeviceId=test-jamesjara;SharedAccessKey=oVoqeN/GGE9qmdPCYBMQhLM82EkM+U7bCICSBq4UB7E=';
+
+var client = clientFromConnectionString(connectionString);
+
+function printResultFor(op) {
+  return function printResult(err, res) {
+    if (err) console.log(op + ' error: ' + err.toString());
+    if (res) console.log(op + ' status: ' + res.constructor.name);
+  };
+}
+
+var connectCallback = function (err) {
+   if (err) {
+     console.log('Could not connect: ' + err);
+   } else {
+     console.log('Client connected');
+
+     // Create a message and send it to the IoT Hub every second
+     setInterval(function(){
+         var temperature = 10 + (Math.random() * 4);
+         var data = JSON.stringify({ deviceId: 'test-jamesjara', temperature: temperature });
+         var message = new Message(data);
+         console.log("Sending message: " + message.getData());
+         client.sendEvent(message, printResultFor('send'));
+     }, 1000);
+   }
+ };
+
+ client.open(connectCallback);
